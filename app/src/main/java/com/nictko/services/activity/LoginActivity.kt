5762ab1eb0e5d68748a5eb28be5e45058508cb59
sarveshjaat <sarveshjat70@gmail.com>
2025-09-20 12:15:17 +0530

    sacsac

--- app/src/main/java/com/nictko/services/activity/LoginActivity.kt
+++ app/src/main/java/com/nictko/services/activity/LoginActivity.kt
@@ -28,7 +28,7 @@ import com.nictko.services.viewmodel.LoginViewModel
 class LoginActivity : AppCompatActivity() {
 
 
-    ///sarveshnewsttaus
+    ///sarveshnewsttaus bffhfhgit
 
     private lateinit var binding: ActivityLoginBinding
     private val viewModel by viewModels<LoginViewModel>()
